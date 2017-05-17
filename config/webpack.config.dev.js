6a12ece8cb3991a2926bf642cc60161ccb7319c5
Rabamidab <rabamidab@gmail.com>
2017-05-17 10:13:36 +0300

    add scss

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -111,6 +111,7 @@ module.exports = {
           // Webpack 2 fixes this, but for now we include this hack.
           // https://github.com/facebookincubator/create-react-app/issues/1713
           /\.(js|jsx)(\?.*)?$/,
+          /\.scss$/,
           /\.css$/,
           /\.json$/,
           /\.svg$/
@@ -139,6 +140,11 @@ module.exports = {
       // "style" loader turns CSS into JS modules that inject <style> tags.
       // In production, we use a plugin to extract that CSS to a file, but
       // in development "style" loader enables hot editing of CSS.
+      {
+        test: /\.scss$/,
+        include: paths.appSrc,
+        loaders: ["style", "css", "sass"]
+      },
       {
         test: /\.css$/,
         loader: 'style!css?importLoaders=1!postcss'
